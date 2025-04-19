6d0296d5ab93fd0f7333e31f3687fad9c565332b
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-20 00:21:11 +0530

    firebase initialized in main & splash done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:mini_task_hub/auth/login_screen.dart';
 import 'package:mini_task_hub/splash_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -15,8 +17,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter To-Do App',
       theme: ThemeData(
       ),
-      //home: SplashScreen()
-      home: LoginScreen(),
+      home: SplashScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_task_hub/auth/login_screen.dart';
+import 'package:mini_task_hub/dashboard/dashboard_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -6,9 +9,15 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
      Future.delayed(
-      const Duration(seconds: 3), () async {
+      const Duration(seconds: 4), () async {
         if (context.mounted){
-
+          final user = FirebaseAuth.instance.currentUser;
+          if (user != null){
+            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => DashboardScreen()));
+          }
+          else {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
+          }
         }
       }
     );
