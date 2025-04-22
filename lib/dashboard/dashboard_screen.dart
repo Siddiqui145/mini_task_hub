ec1cba4df93212fc5f5e608e417f217322046b30
Siddiqui Taha <tahasiddiqui541@gmail.com>
2025-04-22 11:27:47 +0530

    added signout & layout improved well

--- lib/dashboard/dashboard_screen.dart
+++ lib/dashboard/dashboard_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart';
+import 'package:mini_task_hub/auth/login_screen.dart';
 import 'package:mini_task_hub/dashboard/task_details_screen.dart';
+import 'package:mini_task_hub/widgets/custom_messages.dart';
 import 'add_task_screen.dart';
 import 'task_card.dart';
 
@@ -11,6 +13,12 @@ class DashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    Future<void> signout() async {
+      await FirebaseAuth.instance.signOut();
+      showSuccessMessage(context, "Logged out successfully!");
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginScreen()));
+    }
     final user = FirebaseAuth.instance.currentUser;
 
     return Scaffold(
@@ -19,60 +27,55 @@ class DashboardScreen extends StatelessWidget {
         backgroundColor: const Color(0xFF212832),
         elevation: 0,
         toolbarHeight: 90,
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Welcome Back!",
-                    style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                          color: Colors.yellow.shade300,
-                        ),
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    user?.email ?? '',
-                    style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                  ),
-                ],
-              ),
-              const CircleAvatar(
-                radius: 32,
-                backgroundImage: AssetImage('assets/images/profile.jpeg'),
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: ListView(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const SizedBox(height: 50),
-            _buildSectionHeader('Completed Tasks'),
-            const SizedBox(height: 16),
-            _buildTaskStream(
-              context,
-              isCompleted: true,
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome Back!",
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Colors.yellow.shade300,
+                      ),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  user?.email ?? '',
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: Colors.white,
+                      ),
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
             ),
-            const SizedBox(height: 32),
-            _buildSectionHeader('Ongoing Projects'),
-            const SizedBox(height: 16),
-            _buildTaskStream(
-              context,
-              isCompleted: false,
+            const CircleAvatar(
+              radius: 32,
+              backgroundImage: AssetImage('assets/images/profile.jpeg'),
             ),
-            const SizedBox(height: 80),
           ],
         ),
+        actions: [
+          IconButton(onPressed: signout, icon: Icon(Icons.logout))
+        ],
       ),
+      body: Padding(
+  padding: const EdgeInsets.symmetric(horizontal: 16),
+  child: Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      const SizedBox(height: 50),
+      _buildSectionHeader('Completed Tasks'),
+      const SizedBox(height: 16),
+      _buildTaskStream(context, isCompleted: true),
+      const SizedBox(height: 32),
+      _buildSectionHeader('Ongoing Projects'),
+      const SizedBox(height: 16),
+      Expanded(child: _buildTaskStream(context, isCompleted: false)),
+    ],
+  ),
+),
+
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.yellow.shade300,
         onPressed: () {
@@ -82,12 +85,7 @@ class DashboardScreen extends StatelessWidget {
         },
         child: const Icon(Icons.add, color: Colors.black),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: const BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        color: Color(0xFF2A323E),
-        child: SizedBox(height: 60),
-      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 
@@ -142,18 +140,15 @@ class DashboardScreen extends StatelessWidget {
 
         final tasks = snapshot.data!.docs;
 
-        return SizedBox(
-          height: isCompleted ? 170 : 150,
+        return isCompleted ? SizedBox(
+          height: 160,
           child: ListView.builder(
-            scrollDirection: isCompleted ? Axis.horizontal : Axis.vertical,
+            scrollDirection: Axis.horizontal,
             itemCount: tasks.length,
             itemBuilder: (context, index) {
               final task = tasks[index].data() as Map<String, dynamic>;
               final title = task['title'] ?? '';
-              final dueDate = task['taskDateTime'] != null
-                  ? DateFormat('d MMM').format(DateTime.parse(task['taskDateTime']))
-                  : 'N/A';
-              final progress = isCompleted ? 100 : 60; 
+              final progress = 100; 
               final taskId = tasks[index].id;
 
               return GestureDetector(
@@ -165,14 +160,44 @@ class DashboardScreen extends StatelessWidget {
                 child: TaskCard(
                   title: title,
                   label: 'Team members',
-                  dueDate: isCompleted ? null : dueDate,
+                  dueDate:  null,
                   isCompleted: isCompleted,
                   progress: progress,
                 ),
               );
             },
           ),
-        );
+        )
+        : ListView.builder(
+          itemCount: tasks.length,
+            itemBuilder: (context, index) {
+              final task = tasks[index].data() as Map<String, dynamic>;
+              final title = task['title'] ?? '';
+              final dueDate = task['taskDateTime'] != null
+                  ? DateFormat('d MMM').format(DateTime.parse(task['taskDateTime']))
+                  : 'N/A';
+              final progress = 60; 
+              final taskId = tasks[index].id;
+
+              return GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (_) => TaskDetailScreen(taskId: taskId),
+                  ));
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: TaskCard(
+                    title: title,
+                    label: 'Team members',
+                    dueDate: isCompleted ? null : dueDate,
+                    isCompleted: isCompleted,
+                    progress: progress,
+                  ),
+                ),
+              );
+            },
+          );
       },
     );
   }
